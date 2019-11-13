580a02dfc7182b93403d77a145effe41f485c44e
Tal Koren <ardethian@gmail.com>
2019-11-14 00:25:28 +0200

    adding an early return in case no #modals element is present

--- content.js
+++ content.js
@@ -1,16 +1,21 @@
-const modalsElement = document.querySelector('#modals');
-const config = { childList: true, subtree: true };
+(() => {
+  const modalsElement = document.querySelector('#modals');
 
-const callback = mutationsList => {
-  for (let mutation of mutationsList) {
-    const cutleryCheckbox = mutation.target.querySelector('#dont_want_cutlery');
-    
-    if (cutleryCheckbox && !cutleryCheckbox.checked) {
-      cutleryCheckbox.click();
+  if (!modalsElement) return;
+
+  const config = { childList: true, subtree: true };
+
+  const callback = mutationsList => {
+    for (let mutation of mutationsList) {
+      const cutleryCheckbox = mutation.target.querySelector('#dont_want_cutlery');
+      
+      if (cutleryCheckbox && !cutleryCheckbox.checked) {
+        cutleryCheckbox.click();
+      }
     }
   }
-}
 
-const observer = new MutationObserver(callback);
+  const observer = new MutationObserver(callback);
 
-observer.observe(modalsElement, config);
+  observer.observe(modalsElement, config);
+})();
