3076a9ef2464ba68e3a0a228b1adbebf91e6b4b3
Tal Koren <ardethian@gmail.com>
2019-11-14 00:38:28 +0200

    maybe this isn't necessary

--- content.js
+++ content.js
@@ -1,21 +1,17 @@
-(() => {
-  const modalsElement = document.querySelector('#modals');
+const modalsElement = document.querySelector('#modals');
 
-  if (!modalsElement) return;
+const config = { childList: true, subtree: true };
 
-  const config = { childList: true, subtree: true };
-
-  const callback = mutationsList => {
-    for (let mutation of mutationsList) {
-      const cutleryCheckbox = mutation.target.querySelector('#dont_want_cutlery');
-      
-      if (cutleryCheckbox && !cutleryCheckbox.checked) {
-        cutleryCheckbox.click();
-      }
+const callback = mutationsList => {
+  for (let mutation of mutationsList) {
+    const cutleryCheckbox = mutation.target.querySelector('#dont_want_cutlery');
+    
+    if (cutleryCheckbox && !cutleryCheckbox.checked) {
+      cutleryCheckbox.click();
     }
   }
+}
 
-  const observer = new MutationObserver(callback);
+const observer = new MutationObserver(callback);
 
-  observer.observe(modalsElement, config);
-})();
+observer.observe(modalsElement, config);
